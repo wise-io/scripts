a0ca6c4dc371dbe5907a379f76f32d8a974222d2
Aaron Stevenson <7505105+wise-io@users.noreply.github.com>
2023-07-12 19:09:01 -0500

    Create UpdateMicrosoftStoreApps.ps1

---
+++ scripts/UpdateMicrosoftStoreApps.ps1
@@ -0,0 +1,31 @@
+<#
+  .SYNOPSIS
+    Triggers a Microsoft Store update scan.
+  .PARAMETER AutoUpdate
+    Enables automatic Microsoft Store updates.
+  .EXAMPLE
+    UpdateMicrosoftStoreApps.ps1 -AutoUpdate
+#>
+
+param ([switch]$AutoUpdate)
+
+if ($AutoUpdate) {
+  try {
+    $RegPath = 'HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore'
+    if (!(Test-Path $RegPath)) { New-Item -Path $RegPath -Force | Out-Null }
+    New-ItemProperty -Path $RegPath -Name 'AutoDownload' -Value 4 -PropertyType DWORD -Force | Out-Null
+    Write-Output 'Automatic Microsoft Store updates enabled.'
+  }
+  catch { Write-Warning 'Automatic updates were not enabled.' }
+}
+
+try {
+  $AppUpdateResult = Get-CimInstance -Namespace 'Root\cimv2\mdm\dmmap' -ClassName 'MDM_EnterpriseModernAppManagement_AppManagement01' | Invoke-CimMethod -MethodName UpdateScanMethod
+  if ($AppUpdateResult.ReturnValue -ne 0) { $AppUpdateResult }
+  else { Write-Output 'Starting Microsoft Store update scan.' }
+}
+catch { 
+  Write-Warning 'Unable to start Microsoft Store update scan.'
+  Write-Warning $_
+  exit 1
+}
