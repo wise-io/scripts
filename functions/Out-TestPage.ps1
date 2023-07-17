a19786f1a6473e86ee4db6cdeee35277f86c975a
Aaron Stevenson <7505105+wise-io@users.noreply.github.com>
2023-07-17 16:06:39 -0500

    Update Out-TestPage.ps1

--- functions/Out-TestPage.ps1
+++ functions/Out-TestPage.ps1
@@ -1,6 +1,22 @@
-# Sends a test page to the designated printer
 function Out-TestPage {
-  param([String]$Printer)
-  $PrinterObject = Get-WmiObject Win32_Printer | Where-Object { $_.Name -eq $Printer }
-  if ($PrinterObject.PrintTestPage().ReturnValue -eq 0) { Write-Output "Test page sent to: [$Printer]" }
+  <#
+  .SYNOPSIS
+    Sends a test page to the designated printer.
+  .EXAMPLE
+    Out-TestPage -Printer 'Reception Printer'
+  #>
+  param(
+    [Parameter(Position = 0, Mandatory = $true)]
+    [String]$Printer
+  )
+
+  try {
+    $PrinterObject = Get-WmiObject Win32_Printer | Where-Object { $_.Name -eq $Printer }
+    if ($PrinterObject.PrintTestPage().ReturnValue -eq 0) { Write-Output "Test page sent to: [$Printer]" }
+    else { throw }
+  }
+  catch {
+    Write-Warning "Unable to send test page to: [$Printer]"
+    Write-Warning $_
+  }
 }
