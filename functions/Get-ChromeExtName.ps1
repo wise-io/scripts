fbe1fbfcb25f2bd8bb67216267a0740c1d1b3c34
Aaron Stevenson <7505105+wise-io@users.noreply.github.com>
2024-09-30 10:26:07 -0500

    Create Get-ChromeExtName.ps1

---
+++ functions/Get-ChromeExtName.ps1
@@ -0,0 +1,26 @@
+function Get-ChromeExtName {
+  <#
+  .SYNOPSIS
+    Gets the name of a Chrome xxtension using the extension ID
+  .EXAMPLE
+    Get-ChromeExtName -ID 'ghbmnnjooekpmoecnnnilnnbdlolhkhi'
+  #>
+  param(
+    [ValidatePattern('^[a-z]{32}$')]
+    [Parameter(Mandatory = $true)]
+    [String]$ID
+  )
+  
+  $ProgressPreference = 'SilentlyContinue'
+
+  # Fetch extension webpage
+  $URL = "https://chromewebstore.google.com/detail/$ID"
+  try { $Data = Invoke-WebRequest -Uri $URL } catch { }
+
+  # Find title and format
+  [Regex]$Regex = '(?<=og:title" content=")([\S\s]*?)(?=">)'
+  $ExtName = $Regex.Match($Data.Content).Value -replace ' - Chrome Web Store', ''
+  if (($ExtName -eq '') -or ($ExtName -eq 'Chrome Web Store')) { $ExtName = 'Unknown Extension' }
+
+  return $ExtName
+}
