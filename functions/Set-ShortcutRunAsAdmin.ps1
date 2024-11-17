099923c76df0d5a8dbe50fd3f61551d3fd27af4b
Aaron Stevenson <7505105+wise-io@users.noreply.github.com>
2024-11-17 13:53:20 -0600

    Update Set-ShortcutRunAsAdmin.ps1
    Added comment-based help.

--- functions/Set-ShortcutRunAsAdmin.ps1
+++ functions/Set-ShortcutRunAsAdmin.ps1
@@ -1,13 +1,19 @@
 function Set-ShortcutRunAsAdmin {
+  <#
+  .SYNOPSIS
+    Sets a shortcut (.lnk) to run as admin.
+  .EXAMPLE
+    Set-ShortcutRunAsAdmin -Path 'C:\Users\Public\Desktop\Program.lnk'
+  #>
   param(
-      [Parameter(Mandatory = $true)]
-      [String]$Path
+    [Parameter(Mandatory = $true)]
+    [String]$Path
   )
 
   if (Test-Path $Path) {
-      $Bytes = [System.IO.File]::ReadAllBytes($Path)
-      $Bytes[0x15] = $Bytes[0x15] -bor 0x20
-      [System.IO.File]::WriteAllBytes($Path, $Bytes)
+    $Bytes = [System.IO.File]::ReadAllBytes($Path)
+    $Bytes[0x15] = $Bytes[0x15] -bor 0x20
+    [System.IO.File]::WriteAllBytes($Path, $Bytes)
   }
   else { Write-Warning "Path not found: [$Path]" }
 }
