793b459cf82b57f7209e8e0116c0246f7b37e174
Aaron Stevenson <7505105+wise-io@users.noreply.github.com>
2024-11-17 13:48:26 -0600

    Update Convert-IPAddress.ps1
    Added comment-based help.

--- functions/Convert-IPAddress.ps1
+++ functions/Convert-IPAddress.ps1
@@ -1,11 +1,17 @@
 function Convert-IPAddress {
+  <#
+  .SYNOPSIS
+    Converts an IPv4 address to IPv6
+  .EXAMPLE
+    Convert-IPAddress -IPv4 192.168.10.10
+  #>
     param (
         [Parameter(Mandatory = $true)]
         [System.Net.IPAddress]$IPv4
     )
 
     $Octets = $IPv4.IPAddressToString.Split('.')
-    $Hex = $Octets | ForEach-Object { "{0:x2}" -f [int]$_ }
+    $Hex = $Octets | ForEach-Object { '{0:x2}' -f [int]$_ }
     $IPv6 = "::ffff:$($Hex[0..1] -join ''):$($Hex[2..3] -join '')"
 
     return $IPv6
