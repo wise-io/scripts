82bbf2099258b760f1c3b57e74941ecc9ec7012b
Aaron Stevenson <7505105+wise-io@users.noreply.github.com>
2024-11-26 13:05:45 -0600

    Update DisableFastStartup.ps1

--- scripts/DisableFastStartup.ps1
+++ scripts/DisableFastStartup.ps1
@@ -5,5 +5,6 @@ $Value = '0'
 if (!(Test-Path $Path)) {
   New-Item -Path $Path -Force | Out-Null
   New-ItemProperty -Path $Path -Name $Name -Value $Value -PropertyType DWORD -Force | Out-Null
+} else {
+  New-ItemProperty -Path $Path -Name $Name -Value $Value -PropertyType DWORD -Force | Out-Null
 }
-else { New-ItemProperty -Path $Path -Name $Name -Value $Value -PropertyType DWORD -Force | Out-Null }
