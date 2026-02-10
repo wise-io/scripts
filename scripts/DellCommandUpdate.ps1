wise-io/scripts

commit 20e68364e38c19d36c9a263352b618f30bb69d0e

Improves architecture dectection and .NET Desktop Runtime installation logic. Addresses fix proposed in https://github.com/artwertlp-dev/scripts/tree/patch-1.

scripts/DellCommandUpdate.ps1
@@ -32,8 +32,8 @@ function Get-Architecture {
 
   switch ($Architecture.ToLowerInvariant()) {
     { ($_ -eq 'amd64') -or ($_ -eq 'x64') } { return 'x64' }
-    { $_ -eq 'x86' } { return 'x86' }
-    { $_ -eq 'arm' } { return 'arm' }
+    # { $_ -eq 'x86' } { return 'x86' } - DCU 5.X doesn't support 32-bit
+    # { $_ -eq 'arm' } { return 'arm' } - DCU 5.X doesn't support 32-bit ARM
     { $_ -eq 'arm64' } { return 'arm64' }
     default { throw "Architecture '$Architecture' not supported." }
   }
@@ -249,7 +249,7 @@ function Install-DotNetDesktopRuntime {
   }
   
   $LatestDotNet = Get-LatestDotNetDesktopRuntime
-  $CurrentVersion = (Get-InstalledApps -DisplayName 'Microsoft Windows Desktop Runtime').BundleVersion | Where-Object { $_ -like '8.*' }
+  $CurrentVersion = (Get-InstalledApps -DisplayName "Microsoft Windows Desktop Runtime*($Arch)").BundleVersion | Where-Object { $_ -like '8.*' }
   Write-Output "`n.NET 8.0 Desktop Runtime Info`n-----"
   Write-Output "Installed: $CurrentVersion"
   Write-Output "Latest: $($LatestDotNet.Version)"
@@ -280,7 +280,7 @@ function Install-DotNetDesktopRuntime {
     Start-Process -Wait -NoNewWindow -FilePath $Installer -ArgumentList '/install /quiet /norestart'
 
     # Confirm installation
-    $CurrentVersion = (Get-InstalledApps -DisplayName 'Microsoft Windows Desktop Runtime').BundleVersion | Where-Object { $_ -like '8.*' }
+    $CurrentVersion = (Get-InstalledApps -DisplayName "Microsoft Windows Desktop Runtime*($Arch)").BundleVersion | Where-Object { $_ -like '8.*' }
     if ($CurrentVersion -is [system.array]) { $CurrentVersion = $CurrentVersion[0] }
     if ($CurrentVersion -match $LatestDotNet.Version) {
       Write-Output "Successfully installed .NET 8.0 Desktop Runtime [$CurrentVersion]"

commit 1c09a56b21fbed58c486870890f8c017f081098d

Get architecture once. Fixes regression introduced in 20e68364e38c19d36c9a263352b618f30bb69d0e.

scripts/DellCommandUpdate.ps1
@@ -96,7 +96,6 @@ function Install-DellCommandUpdate {
     $DellKBURL = 'https://www.dell.com/support/kbdoc/en-us/000177325/dell-command-update'
   
     # Set fallback URL based on architecture
-    $Arch = Get-Architecture
     if ($Arch -like 'arm*') { 
       $FallbackDownloadURL = 'https://dl.dell.com/FOLDER11914141M/1/Dell-Command-Update-Windows-Universal-Application_6MK0D_WINARM64_5.4.0_A00.EXE'
       $FallbackChecksum = 'b66b27f5c6572574b709591f44c692da5d6954ad7734ba88ac7cb1d08f3ce288'
@@ -223,7 +222,6 @@ function Install-DotNetDesktopRuntime {
     try {
       $BaseURL = 'https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop'
       $Version = (Invoke-WebRequest -Uri "$BaseURL/8.0/latest.version" -UseBasicParsing).Content
-      $Arch = Get-Architecture
       $URL = "$BaseURL/$Version/windowsdesktop-runtime-$Version-win-$Arch.exe"
       $ChecksumURL = "https://dotnet.microsoft.com/en-us/download/dotnet/thank-you/runtime-desktop-$Version-windows-$Arch-installer"
 
@@ -335,6 +333,7 @@ if ((Get-CimInstance -ClassName Win32_BIOS).Manufacturer -notlike '*Dell*') {
 }
 
 # Handle Prerequisites / Dependencies
+$Arch = Get-Architecture
 Remove-DellUpdateApps -DisplayNames 'Dell Update'
 Install-DotNetDesktopRuntime
 